c6e43bfc9073d4e4381025b5438b9bf76a03769d
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-06-09 15:09:39 -0700

    added logic to configure if a number is prime

--- Is it Prime/ViewController.swift
+++ Is it Prime/ViewController.swift
@@ -9,7 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var numLabel: UILabel!
 
+    @IBOutlet var numTextField: UITextField!
+    
+    
+    @IBAction func isPrimeButton(_ sender: Any) {
+        
+        let number = Int(numTextField.text!)
+        
+        func isItPrime(number:Int) -> Bool{
+            var i = 2
+            
+            while i < number{
+                if (number % i) == 0{
+                    
+                    numLabel.text = "\(number) is not Prime"
+                    return false
+                }
+                i = i + 1
+            }
+            numLabel.text =   "\(number) is Prime"
+            return true
+        }
+        print(isItPrime(number: number!))
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
